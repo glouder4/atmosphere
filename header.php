802263c6d908f2ab4b044496d62da192aa5318c0
glouder4 <34104456+glouder4@users.noreply.github.com>
2020-02-02 11:57:00 +0500

    Add files via upload

--- header.php
+++ header.php
@@ -21,27 +21,26 @@
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-		  <a class="navbar-brand" href="#">
-		  	<?php get_template_part( 'template-parts/header/site', 'branding' ); ?>	
-		  </a>
-		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-		    <span class="navbar-toggler-icon"></span>
-		  </button>
-
-		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-		   <?php if ( has_nav_menu( 'menu-1' ) ) : ?>
-				<?php
-						wp_nav_menu(
-							array(
-								'theme_location' => 'menu-1',
-								'menu_class'     => 'main-menu',
-								'items_wrap'     => '<ul id="%1$s" class="%2$s navbar-nav mr-auto">%3$s</ul>',
-							)
-						);
-					?>
-			<?php endif; ?>
-		  </div>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 row">
+		  		<?php get_template_part( 'template-parts/header/site', 'branding' ); ?>	
+		  	
+		  	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		  	  <span class="navbar-toggler-icon"></span>
+		  	</button>
+		  	
+		  	<div class="collapse navbar-collapse col-12 col-sm-12 col-md-8 col-lg-8 col-xl-8 justify-content-end" id="navbarSupportedContent">
+		  	 <?php if ( has_nav_menu( 'menu-1' ) ) : ?>
+		  					<?php
+		  							wp_nav_menu(
+		  								array(
+		  									'theme_location' => 'menu-1',
+		  									'menu_class'     => 'main-menu',
+		  									'items_wrap'     => '<ul id="%1$s" class="%2$s navbar-nav mr-auto">%3$s</ul>',
+		  								)
+		  							);
+		  						?>
+		  				<?php endif; ?>
+		  	</div>
 	</nav>
 	<script type="text/javascript">
 		$(document).ready(function(){

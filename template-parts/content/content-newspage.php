87c3ea014cd4ed705897cbbba070618f7e5999bd
glouder4 <34104456+glouder4@users.noreply.github.com>
2020-02-15 12:10:02 +0500

    Add files via upload

---
+++ template-parts/content/content-newspage.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since 1.0.0
+ */
+
+?>
+
+<?php
+
+$articleClasses = array(
+'col-12',
+'col-sm-12',
+'col-md-10',
+'col-lg-10',
+'col-xl-10'
+);
+?>
+
+<div id="post-<?php the_ID(); ?>" <?php post_class($articleClasses); ?>>
+	<header class="news-header">
+		<?php
+		if ( is_sticky() && is_home() && ! is_paged() ) {
+			printf( '<span class="sticky-post">%s</span>', _x( 'Featured', 'post', 'twentynineteen' ) );
+		}
+		if ( is_singular() ) :
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		else :
+			the_title( sprintf( '<h3 class="entry-title">', esc_url( get_permalink() ) ), '</h3>' );
+		endif;
+		?>
+	</header><!-- .entry-header -->
+
+	<?php twentynineteen_post_thumbnail(); ?>
+
+	<div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+		<?php
+		the_content(
+			sprintf(
+				wp_kses(
+					/* translators: %s: Post title. Only visible to screen readers. */
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentynineteen' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			)
+		);
+
+		wp_link_pages(
+			array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'twentynineteen' ),
+				'after'  => '</div>',
+			)
+		);
+		?>
+	</div><!-- .entry-content -->
+</div><!-- #post-<?php the_ID(); ?> -->
